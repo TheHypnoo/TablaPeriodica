b0a77c2d61579bf4319994c5158e51b26f5ee887
thehypnoo <mysterlegendz@gmail.com>
2020-11-18 23:46:10 +0100

    Vidas cambiado...

--- app/src/main/java/com/sergigonzalez/tablaperiodica/Game.java
+++ app/src/main/java/com/sergigonzalez/tablaperiodica/Game.java
@@ -8,8 +8,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -17,7 +19,11 @@ import android.widget.Toast;
 
 public class Game extends AppCompatActivity {
     TextView enunciadoJuego;
+    TextView Simbolo;
     TextView muestraVidas;
+    ImageView tresCorazones;
+    ImageView dosCorazones;
+    ImageView unCorazon;
     RadioButton bt_radio1;
     RadioButton bt_radio2;
     RadioButton bt_radio3;
@@ -49,7 +55,11 @@ public class Game extends AppCompatActivity {
 
     public void FindID() {
         enunciadoJuego = findViewById(R.id.enunciadoJuego);
+        Simbolo = findViewById(R.id.simboloTextView);
         muestraVidas = findViewById(R.id.Vidas);
+        tresCorazones = findViewById(R.id.trescorazones);
+        dosCorazones = findViewById(R.id.doscorazones);
+        unCorazon = findViewById(R.id.uncorazon);
         bt_radio1 = findViewById(R.id.bt_radio1);
         bt_radio2 = findViewById(R.id.bt_radio2);
         bt_radio3 = findViewById(R.id.bt_radio3);
@@ -79,7 +89,8 @@ public class Game extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     public void preguntaActual(){
         Restante.setText("Restante "+ elementoActual +" / 117");
-        enunciadoJuego.setText("Di el nombre del elemento del simbolo:\n "+allElementos.ArrElemento[elementoActual].getSimbolo());
+        enunciadoJuego.setText("Selecciona cual es el nombre que tiene este Simbolo: ");
+        Simbolo.setText(allElementos.ArrElemento[elementoActual].getSimbolo());
         int numAux = (int) (Math.random()*4+1);
         muestraVidas.setText(String.valueOf(vida));
         if(numAux==1)
@@ -170,8 +181,12 @@ public class Game extends AppCompatActivity {
 
             vida--;
             if(vida == 2) {
+                tresCorazones.setVisibility(View.GONE);
+                dosCorazones.setVisibility(View.VISIBLE);
                 Toast.makeText(Game.this, "Te queda "+vida+" vidas", Toast.LENGTH_SHORT).show();
             } else if(vida == 1) {
+                dosCorazones.setVisibility(View.GONE);
+                unCorazon.setVisibility(View.VISIBLE);
                 Toast.makeText(Game.this, "Cuidado te queda solamente "+vida+" vida!", Toast.LENGTH_SHORT).show();
             } else if(vida == 0) {
                 Toast.makeText(Game.this, "Te has quedado sin vidas!", Toast.LENGTH_SHORT).show();
@@ -185,122 +200,4 @@ public class Game extends AppCompatActivity {
         }
 
     }
-
-    /*
-    public void Game(){
-        int ArrPos = (int)Math.floor(Math.random()*(-(116 + 0))+(116));
-        int ArrPos2 = (int)Math.floor(Math.random()*(-(116 + 0))+(116));
-        int ArrPos3 = (int)Math.floor(Math.random()*(-(116 + 0))+(116));
-        int ArrPos4 = (int)Math.floor(Math.random()*(-(116 + 0))+(116));
-        int aleatorio = (int)Math.floor(Math.random()*(-(10 + 0))+(10));
-        int aleatorio2 = (int)Math.floor(Math.random()*(1-(4+1))+(4));
-        //Hago que no se repitan!
-        if(ArrPos == ArrPos2 || ArrPos == ArrPos3 || ArrPos == ArrPos4) {
-            ArrPos = (int)Math.floor(Math.random()*(0-(116+1))+(116));
-        } else if(ArrPos2 == ArrPos || ArrPos2 == ArrPos3 || ArrPos2 == ArrPos4) {
-            ArrPos2 = (int)Math.floor(Math.random()*(0-(116+1))+(116));
-        } else if(ArrPos3 == ArrPos || ArrPos3 == ArrPos2 || ArrPos3 == ArrPos4) {
-            ArrPos3 = (int)Math.floor(Math.random()*(0-(116+1))+(116));
-        } else if(ArrPos4 == ArrPos || ArrPos4 == ArrPos2 || ArrPos4 == ArrPos3) {
-            ArrPos4 = (int)Math.floor(Math.random()*(0-(116+1))+(116));
-        }
-
-       // if(aleatorio <= 5) {
-            enunciadoJuego.setText("Selecciona cual es el nombre de este Simbolo: "+allElementos.ArrElemento[ArrPos].getSimbolo());
-            if(aleatorio2 == 1) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos].getNombre());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos2].getNombre());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos3].getNombre());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos4].getNombre());
-            } else if(aleatorio2 == 2) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos2].getNombre());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos].getNombre());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos3].getNombre());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos4].getNombre());
-            } else if(aleatorio2 == 3) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos2].getNombre());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos3].getNombre());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos].getNombre());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos4].getNombre());
-            } else if(aleatorio2 == 4) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos2].getNombre());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos3].getNombre());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos4].getNombre());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos].getNombre());
-            }
-        } else if( aleatorio > 5) {
-            enunciadoJuego.setText("Selecciona cual es el Simbolo de este nombre: "+allElementos.ArrElemento[ArrPos].getNombre());
-            if(aleatorio2 == 1) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos].getSimbolo());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos2].getSimbolo());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos3].getSimbolo());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos4].getSimbolo());
-            } else if(aleatorio2 == 2) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos2].getSimbolo());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos].getSimbolo());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos3].getSimbolo());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos4].getSimbolo());
-            } else if(aleatorio2 == 3) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos2].getSimbolo());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos3].getSimbolo());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos].getSimbolo());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos4].getSimbolo());
-            } else if(aleatorio2 == 4) {
-                bt_radio1.setText(allElementos.ArrElemento[ArrPos2].getSimbolo());
-                bt_radio2.setText(allElementos.ArrElemento[ArrPos3].getSimbolo());
-                bt_radio3.setText(allElementos.ArrElemento[ArrPos4].getSimbolo());
-                bt_radio4.setText(allElementos.ArrElemento[ArrPos].getSimbolo());
-            }
-        }
-
-
-
-        int finalArrPos = ArrPos;
-        bt_corregir.setOnClickListener(v -> {
-            if(bt_radio1.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())) {
-                System.out.println("Correcto!1");
-                Game();
-            } else if(bt_radio2.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())){
-                System.out.println("Correcto!2");
-                Game();
-            } else if(bt_radio3.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())) {
-                System.out.println("Correcto!3");
-                Game();
-            } else if(bt_radio4.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())) {
-                System.out.println("Correcto!4");
-                Game();
-            } else if(!bt_radio1.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())) {
-                System.out.println("Incorrecto!1");
-            } else if(!bt_radio2.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())){
-                System.out.println("Incorrecto!2");
-            } else if(!bt_radio3.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())) {
-                System.out.println("Incorrecto!3");
-            } else if(!bt_radio4.getText().toString().equals(allElementos.ArrElemento[finalArrPos].getNombre())) {
-                System.out.println("Incorrecto!4");
-                /*
-            } else if(bt_radio1.getText() == allElementos.ArrElemento[finalArrPos].getSimbolo()) {
-                System.out.println("Correcto!5");
-                Game();
-            } else if(bt_radio2.getText() == allElementos.ArrElemento[finalArrPos].getSimbolo()){
-                System.out.println("Correcto!6");
-                Game();
-            } else if(bt_radio3.getText() == allElementos.ArrElemento[finalArrPos].getSimbolo()) {
-                System.out.println("Correcto!7");
-                Game();
-            } else if(bt_radio4.getText() == allElementos.ArrElemento[finalArrPos].getSimbolo()) {
-                System.out.println("Correcto!8");
-                Game();
-            }else if(bt_radio1.getText() != allElementos.ArrElemento[finalArrPos].getSimbolo()) {
-                System.out.println("Incorrecto!5");
-            } else if(bt_radio2.getText() != allElementos.ArrElemento[finalArrPos].getSimbolo()){
-                System.out.println("Incorrecto!6");
-            } else if(bt_radio3.getText() != allElementos.ArrElemento[finalArrPos].getSimbolo()) {
-                System.out.println("Incorrecto!7");
-            } else if(bt_radio4.getText() != allElementos.ArrElemento[finalArrPos].getSimbolo()) {
-                System.out.println("Incorrecto!8");
-            }
-
-        }});
-
-        */
 }
